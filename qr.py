8390ef3bcceac849a2a439ad7b821113ae50714c
coupido <47298107+coupido@users.noreply.github.com>
2022-01-05 11:33:50 +0100

    added save to docx and pdf

--- qr.py
+++ qr.py
@@ -1,31 +1,60 @@
+import shutil
+from pathlib import Path
+from docx import Document   # pip install python-docx
+from docx.shared import Cm
+import cv2 as cv            # pip install opencv-python
+import docx2pdf
+
 import qrcode
-import cv2 as cv  # pip install opencv-python
+
+
+TEMP = './temp'
+TEMP_WORD = './temp_word'
+
+
+def _create_directory(directory):
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+
+def get_data(filename) -> str:
+    image = cv.imread(filename)
+    detector = cv.QRCodeDetector()
+
+    data, _, _ = detector.detectAndDecode(image)
+    return data
 
 
 class Code:
-    def __init__(self, filename: str):
-        self._filename = filename
-        self._error_correction = qrcode.constants.ERROR_CORRECT_M
+    def __init__(self, data):
+        self._data = data
 
-    def write_data(self, data: str):
-        qr = qrcode.QRCode(error_correction=self._error_correction)
-        qr.add_data(data)
-        qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
-        img.save(self._filename)
+    def _save_image(self, filename, ending):
+        _qr = qrcode.QRCode()
+        _qr.add_data(self._data)
+        _qr.make(fit=True)
+        _img = _qr.make_image(fill_color="black", back_color="white")
+        _img.save(f'{filename}.{ending}')
 
-    def set_error_correction_high(self):
-        self._error_correction = qrcode.constants.ERROR_CORRECT_H
+    def save_jpg(self, filename: str):
+        self._save_image(filename, 'jpg')
 
-    def set_error_correction_medium(self):
-        self._error_correction = qrcode.constants.ERROR_CORRECT_M
+    def save_png(self, filename: str):
+        self._save_image(filename, 'png')
 
-    def set_error_correction_low(self):
-        self._error_correction = qrcode.constants.ERROR_CORRECT_L
+    def save_docx(self, filename: str):
+        document = Document()
+        paragraph = document.add_paragraph()
+        runner = paragraph.add_run()
+        _create_directory(TEMP)
+        for index, date in enumerate(self._data):
+            single_qr = Code(date)
+            single_qr.save_jpg(f'{TEMP}/{index}')
+            runner.add_picture(f'{TEMP}/{index}.jpg', width=Cm(5))
+        document.save(f'./{filename}.docx')
+        shutil.rmtree(TEMP)
 
-    def read_data(self) -> str:
-        image = cv.imread(self._filename)
-        detector = cv.QRCodeDetector()
-
-        data, _, _ = detector.detectAndDecode(image)
-        return data
+    def save_pdf(self, filename: str):
+        _create_directory(TEMP_WORD)
+        self.save_docx(f'{TEMP_WORD}/{filename}')
+        docx2pdf.convert(f'{TEMP_WORD}/{filename}.docx', './')
+        shutil.rmtree(TEMP_WORD)
--- test_qr.py
+++ test_qr.py
@@ -10,23 +10,53 @@ def delete_qr_codes_after_test():
     # Setup: fill with any logic you want
     yield   # this is where the test is happening
     # Teardown : fill with any logic you want
-    jpgs = glob.glob('*.jpg')
-    for file in jpgs:
-        os.remove(file)
+    endings = ['jpg', 'png', 'docx', 'pdf']
+    for ending in endings:
+        files = glob.glob(f'*.{ending}')
+        for file in files:
+            os.remove(file)
 
 
-def test_write_and_read():
+def test_write_and_read_jpg():
     data = "test data"
 
-    qr_code = qr.Code("test.jpg")
-    qr_code.write_data(data)
+    qr_code = qr.Code(data)
+    qr_code.save_jpg('test')
 
-    assert qr_code.read_data() == data
+    assert qr.get_data("test.jpg") == data
 
 
-def test_write_high_error_correction():
+def test_write_and_read_png():
     data = "test data"
 
-    qr_code = qr.Code("test_h.jpg")
-    qr_code.set_error_correction_high()
-    qr_code.write_data(data)
+    qr_code = qr.Code(data)
+    qr_code.save_png('test')
+
+    assert qr.get_data("test.png") == data
+
+
+def test_write_docx_list():
+    data = ['Alan', 'Ada', 'Grace']
+
+    qr_code = qr.Code(data)
+    qr_code.save_docx('test')
+
+    assert os.path.isfile('test.docx')
+
+
+def test_write_docx_single_data():
+    data = 'Alan'
+
+    qr_code = qr.Code(data)
+    qr_code.save_docx('test')
+
+    assert os.path.isfile('test.docx')
+
+
+def test_write_pdf_list_data():
+    data = ['Alan', 'Ada', 'Grace']
+
+    qr_code = qr.Code(data)
+    qr_code.save_pdf('test')
+
+    assert os.path.isfile('test.pdf')
